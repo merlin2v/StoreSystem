a3d6b339c5a7b37c61a75c954c853e400c3ba88d
merlin2v <nathans2v@gmail.com>
2021-01-20 10:59:56 -0800

    Update Client.java

--- src/storesystem/Client.java
+++ src/storesystem/Client.java
@@ -5,6 +5,8 @@
  */
 package storesystem;
 
+import java.util.function.Predicate;
+
 /**
  *
  * @author natha
@@ -16,6 +18,11 @@ public class Client {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Predicate<Integer> f;
+        f = (i) -> {System.out.println("");return false;};
+        
+        
+        
     }
     
 }
