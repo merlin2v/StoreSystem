c849cf492d3e936b5fb27654bb768758f32eb61d
merlin2v <nathans2v@gmail.com>
2021-01-21 11:05:46 -0800

    added some stuff

---
+++ src/storesystem/api/ShoppingCart.java
@@ -0,0 +1,56 @@
+/*
+ * 
+ */
+package storesystem.api;
+
+/**
+ *
+ * @author Tracy
+ */
+public class ShoppingCart {
+
+    public void addItem(Item i) {
+        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    }
+
+    public void addItems(ItemOrder io) {
+        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    }
+
+    public void addItems(Item i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    }
+
+    public void removeItem(Item i) {
+        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    }
+
+    public void removeItems(ItemOrder io) {
+        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    }
+
+    public void removeItems(Item i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    }
+
+    public void alculateTotal() {
+        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    }
+
+    public void completeTransaction() {
+        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    }
+
+    public void IsCompleted() {
+        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    }
+
+    public void calculateFinalTotal() {
+        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    }
+
+    public void calculateTax() {
+        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    }
+    
+}
