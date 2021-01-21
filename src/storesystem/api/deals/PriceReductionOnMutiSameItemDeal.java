5080ab8fa25fa01f16c457e6468f72d9efbc6361
merlin2v <nathans2v@gmail.com>
2021-01-21 13:31:32 -0800

    added Deal

---
+++ src/storesystem/api/deals/PriceReductionOnMutiSameItemDeal.java
@@ -0,0 +1,81 @@
+/*
+ * 
+ */
+package storesystem.api.deals;
+
+import storesystem.api.*;
+
+/**
+ * This represents a price reduction on a specific quantity of a item
+ * @author Nathan
+ */
+public class PriceReductionOnMutiSameItemDeal extends PriceReductionDeal{
+    /**
+     * This is the item to search for
+     */
+    public Item SearchItem;
+    /**
+     * This is the amount required to apply this deal
+     */
+    public int QuantityToRecieve;
+    /**
+     * This is the price to reduce per every {@link QuantityToRecieve}
+     */
+    public double ReducedPrice;    
+    
+    /**
+     * Creates a Deal that can be applied to every {@code quantity} number of 
+     * {@code search} items found in a {@link ShoppingCart}.
+     * @param search the Item to search which is assigned to {@SearchItem}
+     * @param quantity the quantity to reduce the price 
+     * @param reducedPrice the reduced price per item
+     */
+    public PriceReductionOnMutiSameItemDeal(Item search, int quantity, double reducedPrice) {
+        this.SearchItem = search;
+        this.QuantityToRecieve = quantity;
+        this.ReducedPrice = reducedPrice;
+    }
+
+        /**
+     * Creates a Deal that can be applied to every {@code quantity} number of 
+     * {@code search} items found in a {@link ShoppingCart}.
+     * @param search the Item to search which is assigned to {@SearchItem}
+     * @param quantity the quantity to reduce the price 
+     * @param reducedPrice the reduced price per item
+     * @param max the max amount of times this deal can be applied 
+     */
+    public PriceReductionOnMutiSameItemDeal(Item search, int quantity, double reducedPrice, int max) {
+        this.SearchItem = search;
+        this.QuantityToRecieve = quantity;
+        this.ReducedPrice = reducedPrice;
+        this.Max = max;
+        
+        this.Test = (cart, isPeek) -> {
+            if (cart.hasItem(SearchItem)) {
+                ItemOrder order = cart.getItemOrder(SearchItem);
+                if (this.QuantityToRecieve >= order.Quantity) {
+                    //calculations are done here to increase efficiency unless peeked
+                    if (!isPeek) {
+                        // we set TimesApplied here because we use the silent tryReceive
+                        this.TimesApplied = order.Quantity % this.QuantityToRecieve;
+                        if (this.Max!=-1 && this.TimesApplied > this.Max) this.TimesApplied = this.Max;
+                        this.Receivable = this.ReducedPrice * this.TimesApplied; 
+                    }
+                    return true;
+                }else return false;
+                
+            }
+            return false;
+        };
+    }
+
+    @Override
+    public Double tryReceive(ShoppingCart cart) {
+        return super.silentReceive(cart); //we want to silently receive this
+    }
+    
+
+    
+    
+    
+}
