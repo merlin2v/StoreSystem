fa5f823a05dff9b9acfa277353ad3d0dd7c27cef
merlin2v <nathans2v@gmail.com>
2021-01-21 10:50:17 -0800

    added some stuff

--- src/storesystem/api/ItemOrder.java
+++ src/storesystem/api/ItemOrder.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
 package storesystem.api;
 
@@ -11,7 +9,7 @@ package storesystem.api;
  */
 public class ItemOrder {
 
-    
+
 
     
 }
