f7f909d42d7b3946db362b047b8571b28ee00555
merlin2v <nathans2v@gmail.com>
2021-01-21 13:31:37 -0800

    added Deal

---
+++ src/storesystem/api/deals/PriceReductionDeal.java
@@ -0,0 +1,13 @@
+/*
+ *  
+ */
+package storesystem.api.deals;
+
+import storesystem.api.*;
+
+/**
+ * This represents a reduction of price in any way shape or form.
+ * @author Nathan
+ */
+public class PriceReductionDeal extends Deal<Double>{
+}
