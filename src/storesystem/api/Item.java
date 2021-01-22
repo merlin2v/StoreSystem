a293ec27864442460ca6499d377cf6143c22fe0d
Danii-W <Danii-W@users.noreply.github.com>
2021-01-22 14:46:48 -0800

    update item.java

--- src/storesystem/api/Item.java
+++ src/storesystem/api/Item.java
@@ -1,5 +1,5 @@
 /*
- * 
+ * Dani
  */
 package storesystem.api;
 
@@ -8,5 +8,36 @@ package storesystem.api;
  * @author natha
  */
 public class Item {
+	//private fields
+    private String name;
+    private double price;
     
+    //constructors
+    public Item (String name, double price) {
+    	this.name = name;
+    	this.price = price;
+    }
+    
+    public Item() {
+    	this.name = "";
+    	this.price = 0.0;
+    }
+    
+    //getters
+    public String getName() {
+    	return name;
+    }
+    
+    public double getPrice() {
+    	return price;
+    }
+    
+    //setters
+    public void setName(String newName) {
+    	this.name = newName;
+    }
+    
+    public void setPrice(double newPrice) {
+    	this.price = newPrice;
+    }
 }
