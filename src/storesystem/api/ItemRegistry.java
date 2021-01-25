696edad312d61461fc3a5dbac08199a412d5ac13
merlin2v <nathans2v@gmail.com>
2021-01-24 19:33:49 -0800

    updated AdditionalItemsDeal

--- src/storesystem/api/ItemRegistry.java
+++ src/storesystem/api/ItemRegistry.java
@@ -12,19 +12,5 @@ import java.io.Serializable;
  * @author natha
  */
 public class ItemRegistry implements Serializable{
-    @Serial
-    public int value;
-    
-    private void writeObject(java.io.ObjectOutputStream out)
-     throws IOException{
-        
-    }
- private void readObject(java.io.ObjectInputStream in)
-     throws IOException, ClassNotFoundException{
-    
-}
- private void readObjectNoData()
-     throws ObjectStreamException{
-    
-}
+
 }
