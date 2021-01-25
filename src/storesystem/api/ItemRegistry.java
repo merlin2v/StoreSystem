a87cf30387b1d2fbf952a7e276c0ce2646c7009e
Aidryan <77708872+Aidryan@users.noreply.github.com>
2021-01-24 21:05:35 -0800

    Added constructor

--- src/storesystem/api/ItemRegistry.java
+++ src/storesystem/api/ItemRegistry.java
@@ -20,8 +20,8 @@ public class ItemRegistry implements Serializable{
     @Serial
     
     //Private fields
-    private TreeMap<String, Item> registry = new TreeMap<>(); //TreeMap that holds registered items
-    private Set<Deal> deals = new HashSet<>(); //HashSet that holds deals
+    private TreeMap<String, Item> registeredItems; //TreeMap that holds registered items
+    private Set<Deal> deals; //HashSet that holds deals
     
     public int value;
     
@@ -39,51 +39,60 @@ public class ItemRegistry implements Serializable{
 }
  
  //Jenny's code
+ protected ItemRegistry() {
+	 registeredItems = new TreeMap<>();
+	 deals = new HashSet<>();
+ }
+ 
+ //public ItemRegistry getDefaultItemRegistry() {
+	 
+ //}
+ 
  public void registerItem(String name)  { //Registers item into registry with only the name (no price listed)
-	 if (registry.containsKey(name)) {
+	 if (registeredItems.containsKey(name)) {
 		 throw new ItemRegisteredException(); //Throws ItemRegisteredException if the registry already contains the item
 	 } else {
-	 registry.put(name, new Item(name, 0.0));
+	 registeredItems.put(name, new Item(name, 0.0));
 	 }
  }
  
  public void registerItem(String name, double price) { //Registers new item into registry with specified name and price
-	 if (registry.containsKey(name)) {
+	 if (registeredItems.containsKey(name)) {
 		 throw new ItemRegisteredException(); //Throws ItemRegisteredException if the registry already contains the item
 	 } else {
-	 registry.put(name, new Item(name, price));
+	 registeredItems.put(name, new Item(name, price));
 	 }
  }
  
  public void deleteItem(String name) { //Deletes the specified item from the registry
-	 if (!registry.containsKey(name)) {
+	 if (!registeredItems.containsKey(name)) {
 		 throw new ItemNotFoundException(); //Throws ItemNotFoundException if specified item does not exist in registry
 	 } else {
-		 registry.remove(name);
+		 registeredItems.remove(name);
 	 }
  }
  
  public Item getItem(String name) { //Returns Item under specified Key (the name of the item)
-	 if (!registry.containsKey(name)) {
+	 if (!registeredItems.containsKey(name)) {
 		 throw new ItemNotFoundException();
 	 } else {
-		 return registry.get(name);
+		 return registeredItems.get(name);
 	 }
  }
  
  public void setPrice(String name, double price) { //Changes specified item's price
-	 if (!registry.containsKey(name)) {
+	 if (!registeredItems.containsKey(name)) {
 		 throw new ItemNotFoundException(); //Throws ItemNotFoundException if specified item does not exist in registry
 	 } else {
-		 registry.replace(name, new Item(name, price));
+		 registeredItems.replace(name, new Item(name, price));
 	 }
  }
  
  public double havePrice(String name) { //Returns price of specified item
-	 if (!registry.containsKey(name)) {
+	 if (!registeredItems.containsKey(name)) {
 		 throw new ItemNotFoundException(); //Throws ItemNotFoundException if specified item does not exist in registry
 	 } else {
-		 return registry.get(name).getPrice();
+		 return registeredItems.get(name).getPrice();
 	 }
  }
  
