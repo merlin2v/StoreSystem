02ce44964be39427668ff573f29b8c11fb326ea8
tracympham <77692393+tracympham@users.noreply.github.com>
2021-01-26 10:52:15 -0800

    Update ShoppingCart.java

--- src/storesystem/api/ShoppingCart.java
+++ src/storesystem/api/ShoppingCart.java
@@ -1,64 +1,18 @@
-/*
- * 
+/* Tracy Pham 
+ * CS211 - Group #3 Project 1
+ * January 20/2021
+ * Group members: Donovan, Dani, Dat, Jenny, and Nathan
  */
-package storesystem.api;
 
-/**
- *
- * @author Tracy
- */
-public class ShoppingCart {
+import java.util.*;
 
-    public void addItem(Item i) {
-        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
-    }
+public class MainShoppingCart { 
+	// Creates an array list with ItemOrder as the data type
+	// and order as the parameter
+	private ArrayList<ItemOrder> name = new ArrayList<ItemOrder>();
+	// Creates an array list with Integer as the data type 
+	// and order quantity as the parameter
+	private ArrayList<Integer> quantity = new ArrayList<Integer>();
 
-    public void addItems(ItemOrder io) {
-        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
-    }
-
-    public void addItems(Item i, int i0) {
-        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
-    }
-
-    public boolean removeItem(Item i) {
-        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
-    }
-
-    public boolean removeItems(ItemOrder io) {
-        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
-    }
-
-    public boolean removeItems(Item i, int i0) {
-        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
-    }
-
-    public int calculateTotal() {
-        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
-    }
-
-    public void completeTransaction() {
-        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
-    }
-
-    public int isCompleted() {
-        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
-    }
-
-    public int calculateFinalTotal() {
-        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
-    }
-
-    public void calculateTax() {
-        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
-    }
-    
-    public boolean hasItem(Item i) {
-        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
-    }
-    
-    public ItemOrder getItemOrder(Item i) {
-        throw new UnsupportedOperationException("Not supported yet."); //TODO implement
-    }
-    
-}
+	// Creates a shopping cart order based on the name of the items
+	// and the desired quantity
