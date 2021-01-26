212402a70678255585c458e780b0ce7731933163
merlin2v <nathans2v@gmail.com>
2021-01-26 14:47:58 -0800

    created

---
+++ src/storesystem/api/ItemPack.java
@@ -0,0 +1,15 @@
+/*
+ *  
+ */
+package storesystem.api;
+
+/**
+ *
+ * @author Nathan
+ */
+public class ItemPack extends WrappedItem{
+    /**
+     * the pack count of the Item
+     */
+    public int PackCount;
+}
