747eaff21223b44fc9e993371dd9ff7d4ba46e32
merlin2v <nathans2v@gmail.com>
2021-01-27 23:45:42 -0800

    updated javadoc

--- src/storesystem/api/WrappedItem.java
+++ src/storesystem/api/WrappedItem.java
@@ -6,7 +6,10 @@
 package storesystem.api;
 
 /**
- * generic Item wrapper class
+ * Generic Item wrapper class.<br>
+ * This is used to add extra data to Item's and can be done through the use of 
+ * {@link ItemRegistry#registerWrappedItem(java.lang.String, storesystem.api.WrappedItem)} 
+ * which will save the item the the name in the first {@code String} parameter
  * @author Nathan
  */
 public class WrappedItem extends Item{
