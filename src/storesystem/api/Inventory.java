66b5ced22ca6770ae2d3961d00d46eca24f17ac5
merlin2v <nathans2v@gmail.com>
2021-01-29 15:11:52 -0800

    general file cleanup

--- src/storesystem/api/Inventory.java
+++ src/storesystem/api/Inventory.java
@@ -1,14 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  
  */
 package storesystem.api;
 
 import java.io.*;
 import java.util.*;
 /**
- *
+ * This is the object in charge of keeping track of the inventory.
  * @author donovanclay
  */
 
