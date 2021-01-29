596a7a9daf914e43cb3f8e1b7a9ce2172162274f
merlin2v <nathans2v@gmail.com>
2021-01-29 15:28:52 -0800

    improved javadoc

--- src/storesystem/api/WrappedItem.java
+++ src/storesystem/api/WrappedItem.java
@@ -1,14 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  
  */
 package storesystem.api;
 
 /**
  * Generic Item wrapper class.<br>
- * This is used to add extra data to Item's and can be done through the use of 
- * {@link ItemRegistry#registerWrappedItem(java.lang.String, storesystem.api.WrappedItem)} 
+ * This is used to add extra data to Item's and can be done through the the 
+ * {@link ItemRegistry} with the use of 
+ * {@link ItemRegistry#registerWrappedItem(java.lang.String, storesystem.api.WrappedItem) registerWrappedItem()} 
  * which will save the item the the name in the first {@code String} parameter
  * @author Nathan
  */
