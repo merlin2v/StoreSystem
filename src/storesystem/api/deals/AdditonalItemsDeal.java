6def20239ad99e5bc4611d460c2885b10290db2f
merlin2v <nathans2v@gmail.com>
2021-01-29 15:31:00 -0800

    added javadoc

--- src/storesystem/api/deals/AdditonalItemsDeal.java
+++ src/storesystem/api/deals/AdditonalItemsDeal.java
@@ -6,7 +6,8 @@ package storesystem.api.deals;
 import storesystem.api.*;
 
 /**
- *
+ * A class representing additional items to add and take out from stock
+ * TODO Note: the take out from stock feature has not been implemented yet
  * @author Nathan
  */
 public class AdditonalItemsDeal extends Deal<DealObject<ItemOrder[]>> {
