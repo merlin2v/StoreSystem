cf2912befb94f21c45d979d0c75b79987477becb
merlin2v <nathans2v@gmail.com>
2021-01-29 15:51:18 -0800

    fixed package javadoc

--- src/storesystem/api/deals/package-info.java
+++ src/storesystem/api/deals/package-info.java
@@ -2,7 +2,7 @@
  * This package contains Deal objects. These objects are used to implement deals
  * within the system and can be created and added. To add, use a 
  * {@link storesystem.api.ItemRegistry} object to add a 
- * {@link storesystem.api.Deal} object using 
+ * {@link Deal} object using 
  * {@link storesystem.api.ItemRegistry#addDeal(Deal) addDeal(Deal)}.
  */
 package storesystem.api.deals;
