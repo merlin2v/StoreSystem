575b8dc2faed4ce0f052e19bf6e1d25b2ee8babf
merlin2v <nathans2v@gmail.com>
2021-01-29 14:55:32 -0800

    general file cleanup

--- src/storesystem/api/ItemNotFoundException.java
+++ src/storesystem/api/ItemNotFoundException.java
@@ -6,18 +6,18 @@ package storesystem.api;
  */
 @SuppressWarnings("serial")
 public class ItemNotFoundException extends RuntimeException {
-	private String Message = null;
-        /**
-         * creates an ItemNotFoundException
-         */
-	public ItemNotFoundException() {
-		getMessage();
-	}
+    private String Message = null;
+    /**
+     * creates an ItemNotFoundException
+     */
+    public ItemNotFoundException() {
+            getMessage();
+    }
 
-        /**
-         * creates an ItemNotFoundException with a message
-         * @param message the message
-         */
+    /**
+     * creates an ItemNotFoundException with a message
+     * @param message the message
+     */
     public ItemNotFoundException(String message) {
         Message = message;
     }
@@ -26,8 +26,7 @@ public class ItemNotFoundException extends RuntimeException {
      * returns {@code "item not found."} and appends an additional message if any
      * @return the massage of why the exception was thrown
      */
-	public String getMessage() {
-		return "Item not found."+((Message!=null)?Message:"");
-	}
-
+    public String getMessage() {
+        return "Item not found."+((Message!=null)?Message:"");
+    }
 }
