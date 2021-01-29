20c198a74e5a5fff73a949e9d40fcfbe6ec5aabc
merlin2v <nathans2v@gmail.com>
2021-01-29 14:51:32 -0800

    general file cleanup

--- src/storesystem/ClientInterface.java
+++ src/storesystem/ClientInterface.java
@@ -1,13 +1,10 @@
 /*
- 
-* To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+*  
  */
 
 package storesystem;
- import java.util.*;
- import java.io.*;
+import java.util.*;
+import java.io.*;
 import static storesystem.Client.Registry;
 import static storesystem.Client.StoreInventory;
 import storesystem.api.*;
@@ -278,5 +275,4 @@ public class ClientInterface {
             }while(!c.answered());
     }
 
-    
 }
